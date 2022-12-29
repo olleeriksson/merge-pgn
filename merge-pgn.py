f83f9949f591f7ce34cc56497075237dcc19e894
Olle Eriksson <mail@olle-eriksson.com>
2022-12-29 03:31:36 +0100

    Added comment handling

--- merge-pgn.py
+++ merge-pgn.py
@@ -1,11 +1,96 @@
-# Author: Aven Bross
-#
 # Description: A simple tool to merge several pgn games into a single game with
 # variations.
 
 import chess.pgn
 import sys
+import re
+from collections import OrderedDict
 
+def extract_annotations(text):
+    annotations = OrderedDict()
+    normal_text = ""
+
+    # Split the text by placeholders
+    parts = re.split(r"\[|\]", text)
+    for i, part in enumerate(parts):
+        # Check if the current part is a placeholder
+        if part and part[0] == "%":
+            # Split the part by the white space
+            key, values = part.split(" ", 1)
+            # Split the values by the comma and strip any leading/trailing whitespace
+            values = [value.strip() for value in values.split(",")]
+            # Add the values to the annotations dictionary
+            if key not in annotations:
+                annotations[key] = OrderedDict()
+            for value in values:
+                annotations[key][value] = None
+
+        else:
+            # Add the part to the normal_text variable
+            normal_text += part
+
+    # Replace multiple consecutive spaces with a single space in the normal_text variable
+    normal_text = re.sub(r"\ ", " ", normal_text)
+
+    return normal_text, annotations
+
+def merge_text_strings(text1, text2):
+    # If one is included in the other then it's a duplicate comment and should be ignored
+    one_in_two = text1 and text2 and text1 in text2
+    two_in_one = text1 and text2 and text2 in text1
+    combined_text = ""
+
+    combined_text += text1
+    if text1 and text2:
+        combined_text += "\n\n"
+    combined_text += text2
+
+    return combined_text
+
+def merge_comments(text1, text2):
+    normal_text1, annotations1 = extract_annotations(text1)
+    normal_text2, annotations2 = extract_annotations(text2)
+
+    # Concatenate the two normal_text variables separated by two newlines
+    combined_text = merge_text_strings(normal_text1, normal_text2)
+
+    # Merge the values of the annotation maps together
+    for key, values in annotations2.items():
+        if key not in annotations1:
+            annotations1[key] = OrderedDict()
+        for value in values:
+            annotations1[key][value] = None
+
+    # Format the merged annotations in the same way the placeholders appear in the text
+    formatted_annotations = ""
+    for key, values in annotations1.items():
+        formatted_annotations += f"[{key} {','.join(values.keys())}]"
+
+    return combined_text, formatted_annotations
+
+def insert_braces(text):
+    # Find all the occurrances of the braced string using re.finditer
+    brace_matches = list(re.finditer(r'\{(.*?)\}', text, flags=re.DOTALL))
+
+    # Iterate through all the matches (in reversed order because inserting stuff messes up the matches completely)
+    for brace_match in reversed(brace_matches):
+        start_pos = brace_match.start()
+        end_pos = brace_match.end()
+
+        # Find the position of the first [% within the braces
+        percent_match = re.search(r'\[%', brace_match.group(1), flags=re.DOTALL)
+        if percent_match is None:
+            continue
+
+        percent_pos = percent_match.start() + start_pos + 1
+
+        comment_text = text[start_pos + 1:percent_pos].strip()
+
+        # Insert "} {" at the position of the [% , if and only if the text before is non-empty
+        if comment_text:
+            text = text[:percent_pos] + "} { " + text[percent_pos:]
+
+    return text
 
 def main():
     try:
@@ -13,15 +98,18 @@ def main():
     except IndexError:
         raise SystemExit(f"Usage: {sys.argv[0]} <string_to_reverse>")
 
+    master_node = chess.pgn.Game()
+
     games = []
     for name in args:
         pgn = open(name, encoding="utf-8-sig")
         game = chess.pgn.read_game(pgn)
         while game is not None:
+            text, annotations = merge_comments(master_node.comment, game.comment)
+            master_node.comment = f"{text}{annotations}"
+
             games.append(game)
             game = chess.pgn.read_game(pgn)
-
-    master_node = chess.pgn.Game()
 
     mlist = []
     for game in games:
@@ -34,25 +122,38 @@ def main():
         newvars = []
         done = True
         for vnode, nodes in variations:
-            newmoves = {}
+            newmoves = {}  # Maps move to its index in newvars.
             for node in nodes:
                 if node.move is None:
                     continue
                 elif node.move not in list(newmoves):
-                    nvnode = vnode.add_variation(node.move)
+                    nvnode = vnode.add_variation(node.move, nags = node.nags)
+                    text, annotations = merge_comments(node.comment, "")
+                    nvnode.comment = f"{text}{annotations}"
+
                     if len(node.variations) > 0:
                         done = False
                     newvars.append((nvnode, node.variations))
                     newmoves[node.move] = len(newvars) - 1
                 else:
                     nvnode, nlist = newvars[newmoves[node.move]]
+                    text, annotations = merge_comments(nvnode.comment, node.comment)
+                    nvnode.comment = f"{text}{annotations}"
+                    nvnode.nags.update(node.nags)
+
                     if len(node.variations) > 0:
                         done = False
                     nlist.extend(node.variations)
                     newvars[newmoves[node.move]] = (nvnode, nlist)
         variations = newvars
 
-    print(master_node)
+    pgn = f"{master_node}"
+
+    # Workaround to make sure the annotations end up in their own curly brackets,
+    # ie 1.e4 { A comment } { [%cal Ge2e4] }    instead of    1.e4 { A comment [%cal Ge2e4] }
+    pgn = insert_braces(pgn)
+    
+    print(pgn)
 
 
 main()
--- readme.md
+++ readme.md
@@ -1,8 +1,9 @@
-## Merge-PGN
+# Merge-PGN
 
 A simple tool for merging several pgn games into a single pgn with one
-game including all moves as variations. All headers and comments are ignored as
-this is designed only to manipulate the move lines.
+game including all moves as variations.
+
+Based on [merge-pgn](https://github.com/permutationlock/merge-pgn) with the addition of handling comments.
 
 Requires you to have [python-chess](https://python-chess.readthedocs.io)
 installed.
@@ -11,10 +12,47 @@ installed.
 pip install python-chess
 ```
 
-I needed this tool to construct pgn files to upload to ChessTempo's opening
-trainer.
-
 Example:
 ```
 python merge-pgn.py "game1.pgn" "game2.pgn" "games3-7.pgn" > "all_games.pgn"
 ```
+
+Or, one PGN with multiple games:
+```
+python merge-pgn.py "games.pgn" > "all_games_merged.pgn"
+```
+
+## Comments
+
+Merging of comments (including annotations like arrows and circles) are supported
+with a few caveats. But in general it works like this.
+
+The following to PGN's:
+
+```
+1.e4 e5 { This is the first comment } {[%cal Gf2f5][%csl Gb3]}
+```
+
+```
+1.e4 e5 { This is the second comment } {[%cal Ye2e3][%csl Yc4]}
+```
+
+..would be combined into this:
+
+```
+1.e4 e5 { This is the first comment
+
+This is the second comment } { [%cal Gf2f5,Ye2e3][%csl Gb3,Yc4] }
+```
+
+Ie, normal text comments are merged and separated by two newlines. Identical comments 
+are not duplicated.
+
+### Caveats
+
+One might argue that comments should be merged into completely separate {} sections. The PGN standard is vague on this, but most tools do it in this way. However because of a limitation in the chess.pgn library that this tool uses 
+(https://github.com/niklasf/python-chess) that was discussed at 
+https://github.com/niklasf/python-chess/discussions/945 and 
+https://github.com/niklasf/python-chess/issues/946 only annotations ([%cal] and [%csl] etc) have been completely separated into separate {}'s, and even that in a kind of hacky solution.
+
+There are some unhandled issues left, such as when two identical arrows of different colors are found. In that case, both are included in the PGN and left to the viewer to handle, which might not be optimal.
